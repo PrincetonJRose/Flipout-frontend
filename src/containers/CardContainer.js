24eca941bf967a6487270b69fdc142739dd40a13
Darcy Linde <lindedarcy@gmail.com>
2019-05-23 12:07:46 -0400

    installed semantic ui successfully

--- src/containers/CardContainer.js
+++ src/containers/CardContainer.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Card from '../components/Card'
+import {Grid} from 'semantic-ui-react'
 
 function CardContainer(){
   return(
-    <div>
-      <Card />
-    </div>
+    <Grid>
+    <div className="four wide column"><Card /></div>
+    <div className="four wide column"><Card /></div>
+    <div className="four wide column"><Card /></div>
+    <div className="four wide column"><Card /></div>
+    </Grid>
   )
 }
 
