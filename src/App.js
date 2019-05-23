0b9297bc689d9ed6fd501ff32d20972f577a3cc8
Darcy Linde <lindedarcy@gmail.com>
2019-05-23 11:30:54 -0400

    added components and containers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import Menu from './components/Menu'
+import Stats from './components/Stats'
+import Card from './components/Card'
+import Card from './containers/CardContainer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+
+    this.state = {}
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header"></header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
