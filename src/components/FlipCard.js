4ee8d05474859d94341d7a086658551099518611
Darcy Linde <lindedarcy@gmail.com>
2019-05-23 15:51:11 -0400

    reverted from using react reveal

--- src/components/FlipCard.js
+++ src/components/FlipCard.js
@@ -4,14 +4,7 @@ import {Card, Image, Reveal} from 'semantic-ui-react';
 function FlipCard(props){
   return(
     <Card link>
-      <Reveal active animated='move'>
-        <Reveal.Content visible>
-          <Image src={props.image} wrapped/>
-        </Reveal.Content>
-        <Reveal.Content hidden>
-          <Image src={props.image} wrapped/>
-        </Reveal.Content>
-      </Reveal>
+      <Image src={props.image} wrapped />
     </Card>
   )
 }
