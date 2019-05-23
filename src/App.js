d91f7346d104c8094cfb475c88923bce4b90f003
PrincetonJRose <decoyzpr@gmail.com>
2019-05-23 14:10:57 -0400

    Got pokemon data from api, also added another array in state for holding just the pictures. ^_^

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React from 'react';
 import Menu from './components/Menu'
 import CardContainer from './containers/CardContainer'
 import './App.css';
+const pokeURL = `http://localhost:3000/pokemon/`
 
 class App extends React.Component {
   constructor(){
     super()
+    this.state = {
+      pokemon: [],
+      pokemonPics: []
+    }
+  }
 
-    this.state = {}
+  componentDidMount() {
+    fetch(pokeURL)
+    .then(res => res.json())
+    .then(pokemonData => {
+      this.setState({ pokemon: pokemonData })
+      let pics = this.state.pokemon.map( pokemon => {
+        return pokemon.sprites.front
+      })
+      this.setState({ pokemonPics: pics})
+    })
   }
 
   render(){
