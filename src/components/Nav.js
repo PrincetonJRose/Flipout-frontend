a7713066273e3b4e003f3f57ed24d024dd04ca1e
Darcy Linde <lindedarcy@gmail.com>
2019-05-24 10:44:24 -0400

    added menu buttons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { Icon, Menu } from 'semantic-ui-react'
 
-class Nav extends React.Component {
-  render(){
-    return(
-      null
+class Nav extends Component {
+  state = {}
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Menu icon='labeled'>
+        <Menu.Item name='flag checkered' active={activeItem === 'flag checkered'} onClick={this.handleItemClick}>
+          <Icon name='flag checkered' />
+          New Game
+        </Menu.Item>
+
+        <Menu.Item
+          name='trophy'
+          active={activeItem === 'trophy'}
+          onClick={this.handleItemClick}
+        >
+          <Icon name='trophy' />
+          High Scores
+        </Menu.Item>
+
+        <Menu.Item
+          name='clipboard'
+          active={activeItem === 'clipboard'}
+          onClick={this.handleItemClick}
+        >
+          <Icon name='clipboard' />
+          Settings
+        </Menu.Item>
+
+        <Menu.Item
+          name='power off'
+          active={activeItem === 'power off'}
+          onClick={this.handleItemClick}
+        >
+          <Icon name='power off' />
+          Logout
+        </Menu.Item>
+      </Menu>
     )
   }
 }
